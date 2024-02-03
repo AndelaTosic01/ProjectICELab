29bb55dadbb7a9a9d8d1eed90a74510c53132d1d
AndelaTosic01 <158750808+AndelaTosic01@users.noreply.github.com>
2024-02-03 15:40:59 +0100

    Add files via upload

---
+++ MintermediateRepresentationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MintermediateRepresentationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
