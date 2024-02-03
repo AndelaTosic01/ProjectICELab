a4357e3b87ea6e78a2b32411d1ddf208cce1b0dc
AndelaTosic01 <158750808+AndelaTosic01@users.noreply.github.com>
2024-02-03 15:50:04 +0100

    Add files via upload

---
+++ MIntermediateRepresentation/RVSDG/Exceptions/IrregularInputException.cs
@@ -0,0 +1,17 @@
+﻿using MIntermediateRepresentation.RVSDG.DataDeclarations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIntermediateRepresentation.RVSDG.Exceptions
+{
+    public class IrregularInputException : ArgumentException
+    {
+        public IrregularInputException() : base("Input is not correct.") { }
+        public IrregularInputException(string message) : base(message) { }
+
+        public IrregularInputException(object? constValue) : base(String.Format("{0} is a constant.", constValue)) { }
+    }
+}
