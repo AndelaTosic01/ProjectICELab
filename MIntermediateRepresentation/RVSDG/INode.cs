9d781e77bf5dea665b27452464c5d2ec18927e11
AndelaTosic01 <158750808+AndelaTosic01@users.noreply.github.com>
2024-02-03 15:47:28 +0100

    Delete MIntermediateRepresentation/RVSDG directory

--- MIntermediateRepresentation/RVSDG/INode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MIntermediateRepresentation.RVSDG
-{
-    public interface INode
-    {
-        public List<IInput> Inputs { get; }
-        public List<IOutput> Output { get; }
-        public string Name { get; }
-    }
-}
--- MIntermediateRepresentation/RVSDG/Theta.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MIntermediateRepresentation.RVSDG
-{
-    public class Theta : Region
-    {
-        private static readonly string _name = "Theta";
-        public override string Name => _name;
-        public Theta(List<Node> nodesInDelta, IOutput[] output, params IInput[] inputs) : base(nodesInDelta, output, inputs) { }
-    }
-}
