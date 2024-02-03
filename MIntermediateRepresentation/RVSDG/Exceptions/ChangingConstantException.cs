c3e53be2a74fc79695c4d92078847130ea2549ec
AndelaTosic01 <158750808+AndelaTosic01@users.noreply.github.com>
2024-02-03 15:36:12 +0100

    Add files via upload

---
+++ MIntermediateRepresentation/RVSDG/Exceptions/ChangingConstantException.cs
@@ -0,0 +1,17 @@
+﻿using MIntermediateRepresentation.RVSDG.DataDeclarations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIntermediateRepresentation.RVSDG.Exceptions
+{
+    public class ChangingConstantException : Exception
+    {
+        public ChangingConstantException() : base("The value is a constant and it is not possibile to modify.") { }
+        public ChangingConstantException(string message) : base(message) { }
+
+        public ChangingConstantException(object? constValue) : base(String.Format("{0} is a constant.", constValue)) {}
+    }
+}
