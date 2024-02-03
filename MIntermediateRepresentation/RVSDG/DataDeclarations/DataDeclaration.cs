ae4a009d875c49439612bbcb679fe1f4fb789ecb
AndelaTosic01 <158750808+AndelaTosic01@users.noreply.github.com>
2024-02-03 15:35:28 +0100

    Add files via upload

---
+++ MIntermediateRepresentation/RVSDG/DataDeclarations/DataDeclaration.cs
@@ -0,0 +1,11 @@
+﻿using MIntermediateRepresentation.RVSDG.Types;
+
+namespace MIntermediateRepresentation.RVSDG.DataDeclarations
+{
+    public abstract class DataDeclaration
+    {
+        public abstract string Name { get; }
+        public abstract object Value { get; set; }
+        public abstract IDataType Type { get; }
+    }
+}
