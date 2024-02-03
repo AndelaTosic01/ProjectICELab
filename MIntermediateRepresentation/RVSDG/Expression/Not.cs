7f0392456429a0738914c9397608db8d5391b541
AndelaTosic01 <158750808+AndelaTosic01@users.noreply.github.com>
2024-02-03 15:37:27 +0100

    Add files via upload

---
+++ MIntermediateRepresentation/RVSDG/Expression/Not.cs
@@ -0,0 +1,40 @@
+﻿using MIntermediateRepresentation.RVSDG.DataDeclarations;
+using MIntermediateRepresentation.RVSDG.Types;
+using MIntermediateRepresentation.RVSDG.Exceptions;
+
+namespace MIntermediateRepresentation.RVSDG.Expression
+{
+    public class Not : Operation
+    {
+        private static readonly string _name = "NOT";
+        public override string Name => _name;
+
+        public Not(IOutput[] output, IInput[] input) : base(output, input)
+        {
+            if (input[0] == null || input.Length != 1)
+                throw new IrregularInputException("Input must contains only 1 element.");
+
+            if (output[0] == null || output.Length != 1 || ((Edge)output[0]).Type is not (IntegerType or BooleanType))
+                throw new IrregularOutputException("Output variable must be of type IntegerType or BooleanType.");
+
+            CheckInputTypeCompatibility();
+        }
+
+        public override void CalcResult()
+        {
+            IDataType type = ((Edge)Output[0]).Type;
+            if (type is BooleanType)
+            {
+                Output[0].SetValue(!BooleanType.CastValue(Inputs[0].GetValue()));
+            }
+            if (type is IntegerType)
+            {
+                var val = IntegerType.CastValue(Inputs[0].GetValue());
+                if (val != 0)
+                    Output[0].SetValue(0);
+                else
+                    Output[0].SetValue(1);
+            }
+        }
+    }
+}
---
+++ MIntermediateRepresentation/RVSDG/Expression/Or.cs
@@ -0,0 +1,48 @@
+﻿using MIntermediateRepresentation.RVSDG.DataDeclarations;
+using MIntermediateRepresentation.RVSDG.Types;
+using MIntermediateRepresentation.RVSDG.Exceptions;
+
+namespace MIntermediateRepresentation.RVSDG.Expression
+{
+    public class Or : Operation
+    {
+        private readonly static string _name = "OR";
+        public override string Name => _name;
+        public Or(IOutput[] output, IInput[] input) : base(output, input)
+        {
+            if (input == null || input.Length < 2)
+                throw new IrregularInputException("Input must contain at least 2 elements.");
+
+            if (output == null || output.Length != 1 || ((Edge)output[0]).Type is not (IntegerType or BooleanType))
+                throw new IrregularOutputException("Output variable must be of type IntegerType or BooleanType.");
+            
+            CheckInputTypeCompatibility();
+        }
+        public override void CalcResult()
+        {
+            IDataType type = ((Edge)Output[0]).Type;
+            if (type is BooleanType)
+            {
+                var or = BooleanType.CastValue(Inputs[0].GetValue());
+                for (int i = 1; i < Inputs.Count; i++)
+                {
+                    or |= BooleanType.CastValue(Inputs[i].GetValue());
+                    if (or)
+                    {
+                        break;
+                    }
+                }
+                Output[0].SetValue(or);
+            }
+            else if (type is IntegerType)
+            {
+                var or = IntegerType.CastValue(Inputs[0].GetValue());
+                for (int i = 1; i < Inputs.Count; i++)
+                {
+                    or |= IntegerType.CastValue(Inputs[i].GetValue());
+                }
+                Output[0].SetValue(or);
+            }
+        }
+    }
+}
